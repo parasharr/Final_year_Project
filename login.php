8dba1dbc943e32fcd6a3f12c9c66a7a0320fa49f
PranjeetGoswami <prgoswami2020@gmail.com>
2024-09-07 09:18:31 +0530

    update to repo

--- login.php
+++ login.php
@@ -32,7 +32,7 @@ if(isset($_POST["submit"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecommerce website</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-log.css">
     <link rel="stylesheet" href="https://fontawesome.com/releases/v6.04.0/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -77,12 +77,6 @@ if(isset($_POST["submit"])){
     </form>
     </div>
  
-
-    <footer class="section-p1">
-        <div class="copyright">
-            <p>2024, Pranjeet etc - HTML CSS Ecommerce Tamplate</p>
-        </div>
-    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
--- register.php
+++ register.php
@@ -35,7 +35,7 @@ if(isset($_POST["submit"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecommerce website</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-reg.css">
     <link rel="stylesheet" href="https://fontawesome.com/releases/v6.04.0/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -62,8 +62,8 @@ if(isset($_POST["submit"])){
         </div>
     </section>
 
-
-    <div class="form-box">
+<section id="form-bg">
+<div class="form-box">
         <h1 id="title">Sign Up</h1>
         <form class="" action="" method="post" autocomplete="off">
             <div class="input-group">
@@ -92,16 +92,9 @@ if(isset($_POST["submit"])){
             </div>
     </form>
     </div>
-   
-   
+</section>
     
-
-    <footer class="section-p1">
-        <div class="copyright">
-            <p>2024, Pranjeet etc - HTML CSS Ecommerce Tamplate</p>
-        </div>
-    </footer>
-
+   
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
