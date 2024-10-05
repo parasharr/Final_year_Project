fed11dac3946528448568e329bec2ee522c21fda
PranjeetGoswami <prgoswami2020@gmail.com>
2024-10-05 08:32:04 +0530

    add to repo

--- payment.php
+++ payment.php
@@ -44,32 +44,72 @@ require "config.php";
         </div>
     </section>
 
-    <section id="pay-card" class="section-p1">
-        <div class="border">
+    <section id="product1" class="section-p1">
+        <div class="pro-container-1">
         <?php
-$select_cart = mysqli_query($conn, "Select * from `cart`");
+                $select_cart = mysqli_query($conn, "Select * from `cart`");
+                if(mysqli_num_rows($select_cart)>0){
+                while($fetch_product = mysqli_fetch_assoc($select_cart)){
+            ?>
 
+        
+            <div class="order">
+            <form method="post" action="">
+            <img class="img-5" src="images/<?php echo $fetch_product["image"]?>" alt="pro-img">
+                    <div class="description">
+                        <h3 class="one"><?php echo $fetch_product["name"]; ?></h3>
+                        <span>₹<?php echo $fetch_product["price"] ?></span><br>
+                        <span>Quantity: <?php echo $fetch_product["quantity"] ?></span><br><br>
+                        <span>Seller: Glam Boutique</span>
+                        <input type="hidden" name="products_name" value="<?php echo $fetch_product['name']; ?>"> 
+                    </div>
+            </form>
+            </div>
+<?php
+    }
+}
+?>    
+    </div>
+</section>    
+
+
+    <section id="custom-details" class="section-p1">
+    <?php
+$select_customer = mysqli_query($conn, "Select * from `customer`");
+
+if(mysqli_num_rows($select_customer) > 0){
+    while($fetch_details = mysqli_fetch_assoc($select_customer)){
+        ?>
+        <div class="custom-det">
+            <h3 class="address">Shipping to:<?php echo $fetch_details['fullname'], $fetch_details['address'], $fetch_details['city']?></h3>
+            <hr>
+            <br>
+            <?php
+$select_cart = mysqli_query($conn, "Select * from `cart`");
+$all_total = 0;
 if(mysqli_num_rows($select_cart)>0){
     while($fetch_product = mysqli_fetch_assoc($select_cart)){
-        ?>
-        <div class="img-cls">
-                <img class="img-5" src="images/<?php echo $fetch_product["image"]?>" alt="pro-img">
-            </div>
-                <div class="pro-det">
-                    <h3 class="one"><?php echo $fetch_product["name"]; ?></h3>
-                    <span class="one">Quantity: <?php echo $fetch_product["quantity"]; ?></span>
-                    <p class="one">Price: ₹<?php echo $subtotal=number_format($fetch_product['price']*$fetch_product['quantity']); ?></p>
-                    <h4 class="one">Order ID: <?php echo $fetch_product["id"] ?></h4>
-                    <span class="one">Discount: 40%</span>
-                    <h4 class="one">Free shipping</h4>
-                <p class="one">Seller: Glam Boutique</p>
-            </div>
+    
+        $all_total = $all_total+($fetch_product['price']*$fetch_product['quantity']);
+            
+    }
+}
+?>  
+    <p class="price">Discount: 40%</p>
+    <p class="price">Items Price: ₹<?php echo $subtotal=number_format($all_total); ?></p>
+    <p class="price">Delivery: <strong style="color: green;">Free</strong></p>
+    <hr>
+    <br>
+    <div>
+            <h3 class="price">Order Total: ₹<?php echo $subtotal=number_format($all_total); ?></h3>
+    </div>
+        </div>
         <?php
     }
 }
 ?>
-        </div>
-    </section>    
+    </section>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
